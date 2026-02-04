79f0adfc862cae0465b738eab2a1f41547b9e850
beestcode <153935496+beestcode@users.noreply.github.com>
2026-02-05 03:28:55 +0500

    feat: add alternative function to remove duplicates using Set

--- Array/remove_duplicates.js
+++ Array/remove_duplicates.js
@@ -8,3 +8,9 @@ function duplicate(val){
     console.log(arr)
 }
 duplicate([1,1,2,3,4,5])
+
+//using set
+function duplicate1(val){
+    return [ ...new Set(val)]
+}
+console.log(duplicate1([1,1,2,3,4,5]))
