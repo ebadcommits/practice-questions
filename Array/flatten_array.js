41e8784f69d8fa577c428b6de604929df66f409a
beestcode <153935496+beestcode@users.noreply.github.com>
2026-02-11 02:37:24 +0500

    feat: implement flatten function to recursively flatten nested arrays

---
+++ Array/flatten_array.js
@@ -0,0 +1,13 @@
+function flatten(arr){
+    let array = []
+    for(let elem of arr){
+        if(Array.isArray(elem)){
+            array.push(...flatten(elem))
+        }
+        else{
+            array.push(elem)
+        }
+    }
+    return array.sort()
+}
+console.log(flatten([8,9,[3,4,[5,6,[7,1],2]]]))
