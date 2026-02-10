2b3fe6260c595a374944bfd1872c359fd68478b4
beestcode <153935496+beestcode@users.noreply.github.com>
2026-02-11 02:41:05 +0500

    feat: add flat function to utilize built-in array flattening method

--- Array/flatten_array.js
+++ Array/flatten_array.js
@@ -11,3 +11,9 @@ function flatten(arr){
     return array.sort()
 }
 console.log(flatten([8,9,[3,4,[5,6,[7,1],2]]]))
+
+//built in function
+function flat(arr){
+    return arr.flat()
+}
+console.log(flat([1,2,[3,4]]))
