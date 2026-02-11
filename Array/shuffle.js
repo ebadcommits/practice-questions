49b7dd656ea7d53ebcd230f2fec792b84314e91c
beestcode <153935496+beestcode@users.noreply.github.com>
2026-02-11 15:08:38 +0500

    feat: add shuffle function to randomly reorder elements in an array

---
+++ Array/shuffle.js
@@ -0,0 +1,13 @@
+const shuffle = function(arr){
+    let shuffled = arr
+    for(let i = arr.length - 1; i>0; i--){
+       let random = Math.floor(Math.random() * i);
+        
+       let temp = shuffled[i]
+        shuffled[i] = shuffled[random]
+        shuffled[random] = temp
+        
+    }
+    return shuffled
+}
+console.log(shuffle([5,4,8,3,2,9,0,1]))
