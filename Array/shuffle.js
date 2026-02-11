190ea1c1bc4eb9a70873f36bfd2efe523e720eda
beestcode <153935496+beestcode@users.noreply.github.com>
2026-02-11 15:25:43 +0500

    fix: correct random index calculation in shuffle function

--- Array/shuffle.js
+++ Array/shuffle.js
@@ -1,7 +1,7 @@
 const shuffle = function(arr){
     let shuffled = arr
     for(let i = arr.length - 1; i>0; i--){
-       let random = Math.floor(Math.random() * i);
+       let random = Math.floor(Math.random() * (i+1));
         
        let temp = shuffled[i]
         shuffled[i] = shuffled[random]
