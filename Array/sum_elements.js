118016515a997e008bb5e2ab4404d066f31cac9e
beestcode <153935496+beestcode@users.noreply.github.com>
2026-02-12 23:32:04 +0500

    refactor: rename sum function to sum1 and update test cases

--- Array/sum_elements.js
+++ Array/sum_elements.js
@@ -1,11 +1,11 @@
-function sum(arr){
+function sum1(arr){
     let sum = 0
     for(let e of arr){
         sum += e
     }
     console.log(sum)
 }
-sum([1,2,3,4,5])
+sum1([1,2,3,4,5])
 
 //map
 function sum2(arr){
@@ -13,7 +13,7 @@ function sum2(arr){
     arr.map(num => sum += num)
     console.log(sum)
 }
-sum2([1,2,3,4,5])
+sum2([1,2,3,4,5,6])
 
 //foreach
 function sum(arr) {
@@ -21,3 +21,10 @@ function sum(arr) {
     arr.forEach(num => total += num);
     return total;
 }
+console.log(sum([1,2,3,4,5,6,7]))
+
+//reduce
+function sum(arr){
+    return arr.reduce((acc , curr) => acc + curr, 0)
+}
+console.log(sum([1,2,3,4,5,6,7,8]))
