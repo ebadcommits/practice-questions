ecb30287e064d50a3b7e45bb61418383024bc7d2
beestcode <153935496+beestcode@users.noreply.github.com>
2026-02-12 22:00:57 +0500

    feat: update sort function to accept 'asc' and 'desc' as parameters

--- Array/sorting.js
+++ Array/sorting.js
@@ -1,5 +1,5 @@
-const sort = function(arr, order){
-    if(order === "ascending") return arr.sort((a, b) => a - b)
-    if(order === "descending") return arr.sort((a, b) => b - a)   
+const sort = function(arr, order = "asc"){
+    order.toLowerCase()
+    return order === "asc" ? arr.sort((a, b) => a - b) : arr.sort((a, b) => b - a)   
 }
-console.log(sort([5,6,7,9,4,1,2,8], "ascending"))
+console.log(sort([5,6,7,9,4,1,2,8], "DESCENDING"))
