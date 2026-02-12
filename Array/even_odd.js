fb8aaf32da74f311c1347dc89bccd62cc8f50d88
beestcode <153935496+beestcode@users.noreply.github.com>
2026-02-12 23:54:31 +0500

    feat: add additional functions for separating even and odd numbers using map and reduce

--- Array/even_odd.js
+++ Array/even_odd.js
@@ -8,3 +8,21 @@ function evenodd(arr){
 }
 evenodd([1,2,3,4,5,6,7,8,9,10])
 
+//map
+function evenodd2(arr){
+    let even = []
+    let odd = []
+    
+    arr.map((num) => num % 2 === 0 ? even.push(num) : odd.push(num))
+    console.log(even, odd)
+}
+evenodd2([1,2,3,4,5,6,7,8,9,10])
+
+//reduce
+function evenodd3(arr) {
+    return arr.reduce((acc, num) => {
+        acc[num % 2 === 0 ? "even" : "odd"].push(num);
+        return acc;
+    }, { even: [], odd: [] });
+}
+console.log(evenodd3([1,2,3,4,5,6,7,8,9,10]));
