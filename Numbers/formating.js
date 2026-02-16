f962b4dd1d8c96ae0f8a2153ce43e90511732d07
beestcode <153935496+beestcode@users.noreply.github.com>
2026-02-17 03:58:23 +0500

    adding commas by reversing

--- Numbers/formating.js
+++ Numbers/formating.js
@@ -1,10 +1,15 @@
 function format(num){
+    let altered = []
     let reversed = num.split('').reverse()
-    for(let i = 0; i < num.length; i++){
-        if(i % 3 === 0){
-            reversed.join(',')
+    for(let i = 0; i < reversed.length; i++){
+        if(i % 3 === 2){
+            altered.push(reversed[i], ',')
+        }
+        else{
+            altered.push(reversed[i])
         }
     }
-    console.log(reversed)
+    let flipped = altered.reverse().join('')
+    console.log(flipped)
 }
 format("1234567")
