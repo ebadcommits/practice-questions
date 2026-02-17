51abfd8fa48cf574625e6718479c22d9bf8166f0
beestcode <153935496+beestcode@users.noreply.github.com>
2026-02-18 03:54:52 +0500

    return random

--- Numbers/random.js
+++ Numbers/random.js
@@ -3,3 +3,10 @@ function random(max, min){
     console.log(rndmnum)
 }
 random(5,10)
+
+
+//return
+function rand(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+console.log(rand(50,80))
