ebf9755eab105196817cc0de3f12f32a9820bc22
beestcode <153935496+beestcode@users.noreply.github.com>
2026-02-28 03:36:55 +0500

    add moveZeros function to swap zeros to the end of the array

--- Logic and decision/zero_to_end.js
+++ Logic and decision/zero_to_end.js
@@ -13,3 +13,23 @@ function zero(num){
     console.log(nonzero)
 }
 zero([0,2,0,4,5])
+
+//swapping
+function moveZeros(arr) {
+    let pos = 0; // position for next non-zero to go
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== 0) {
+            // swap arr[i] with arr[pos]
+            let temp = arr[pos];
+            arr[pos] = arr[i];
+            arr[i] = temp;
+
+            pos++; // move the pointer
+        }
+    }
+
+    return arr;
+}
+
+console.log(moveZeros([0, 1, 0, 3, 12]));
