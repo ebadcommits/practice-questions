09d9fb917ea61b1641560c10a93b85511c22abf8
beestcode <153935496+beestcode@users.noreply.github.com>
2026-03-01 23:55:20 +0500

    add balanced function to check for balanced brackets in a string

---
+++ Logic and decision/balanced_brackets.js
@@ -0,0 +1,29 @@
+function balanced(str) {
+    let stack = [];
+    let pairs = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let char of str) {
+
+        // if opening bracket → push
+        if (char === '(' || char === '{' || char === '[') {
+            stack.push(char);
+        }
+
+        // if closing bracket
+        else if (char === ')' || char === '}' || char === ']') {
+            // stack must have that opening bracket on top
+            if (stack.pop() !== pairs[char]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+}
+
+console.log(balanced("{[()]}"));  // true
+console.log(balanced("{[(])}"));  // false
