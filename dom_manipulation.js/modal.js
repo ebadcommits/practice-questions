e62f55eac893442ad4699c9e809156cb41a6ffee
beestcode <153935496+beestcode@users.noreply.github.com>
2026-03-12 04:56:45 +0500

    add initial HTML and JavaScript files for modal functionality

---
+++ dom_manipulation.js/modal.js
@@ -0,0 +1,7 @@
+document.querySelector('#openModal').addEventListener("click", function(){
+    document.querySelector("#modal").style.display = 'flex'
+})
+
+document.querySelector('#closeModal').addEventListener("click", function(){
+    document.querySelector("#modal").style.display = 'none'
+})
