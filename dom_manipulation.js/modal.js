dc47c011f2136ac7fd95f1e4b9eb750fb283c83c
beestcode <153935496+beestcode@users.noreply.github.com>
2026-03-12 15:10:29 +0500

    add event listeners for closing modal on outside click and Escape key

--- dom_manipulation.js/modal.js
+++ dom_manipulation.js/modal.js
@@ -5,3 +5,17 @@ document.querySelector('#openModal').addEventListener("click", function(){
 document.querySelector('#closeModal').addEventListener("click", function(){
     document.querySelector("#modal").style.display = 'none'
 })
+
+//if press out of the modal
+document.querySelector('#modal').addEventListener('click', function(e){
+    if(e.target === this){
+        this.style.display = 'none'
+    }
+})
+
+//if press esc key
+document.addEventListener('keydown', function(e){
+    if(e.key === 'Escape'){
+        document.querySelector('#modal').style.display = 'none'
+    }
+})
