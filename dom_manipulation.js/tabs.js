b9551dde6a032fdd921a3e5d58804a59aace3e37
ebad sheikh <ibbeescode@gmail.com>
2026-03-24 02:11:40 +0500

    refactor: streamline tab click event handler in tabs.js

--- dom_manipulation.js/tabs.js
+++ dom_manipulation.js/tabs.js
@@ -2,22 +2,14 @@ const tabs = document.querySelectorAll('.tab');
 const contents = document.querySelectorAll('.content');
 
 tabs.forEach(tab => {
-    tab.addEventListener('click', function () {
+    tab.addEventListener('click', function(){
+        tabs.forEach(t => t.classList.remove('active'))
+        contents.forEach(c => c.classList.remove('active'))
 
-        // 1. remove active from all tabs
-        tabs.forEach(t => t.classList.remove('active'));
+        tab.classList.add('active')
 
-        // 2. remove active from all contents
-        contents.forEach(c => c.classList.remove('active'));
+        const tabdata = tab.dataset.tab
 
-        // 3. activate clicked tab
-        tab.classList.add('active');
-
-        // 4. get the number of this tab
-        const tabNumber = tab.dataset.tab;
-
-        // 5. find the matching content and show it
-        document.querySelector(`.content[data-content="${tabNumber}"]`)
-            .classList.add('active');
-    });
-});
+        document.querySelector(`.content[data-content = "${tabdata}"]`).classList.add('active')
+    } )
+})
